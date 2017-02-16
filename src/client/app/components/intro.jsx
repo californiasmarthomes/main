a3b67d053b7db9c7ebb9fba195a866f9b98c0c3c
Eric Metens <em@bolt.com>
2017-02-16 14:48:03 -0800

    iterate intro

--- src/client/app/components/intro.jsx
+++ src/client/app/components/intro.jsx
@@ -1,22 +1,22 @@
 import {render} from 'react-dom';
-import intro_background from 'images/intro_background.jpg'
 
 export default class Intro extends React.Component {
   render () {
     return (
-      <div className="section intro">
+      <div className="intro">
         <div class="row">
-          <div className="title col s12 hide-on-large-only">
-              Bring the Smarts to your Home
-          </div>
-          <div className="title col s12 hide-on-med-and-down">
-              Bring the Smarts to your Home
+          <div className="overlay">
+            <div className="title col s12 hide-on-large-only">
+                Bring the Smarts to your Home
+            </div>
+            <div className="title col s12 hide-on-med-and-down">
+                Bring the Smarts to your Home
+            </div>
+            <div className="container content">
+              Smart Home consultation and installation by a knowledgeable local professional.
+            </div>
           </div>
         </div>
-        <div className="container content">
-          Smart Home consultation and installation by a knowledgeable local professional.
-        </div>
-        <img className="responsive-img" src={intro_background} />
         <div>Voice control</div>
         <div>Lighting</div>
         <div>Climate Control (thermostats, fans)</div>
--- src/client/app/components/navbar.jsx
+++ src/client/app/components/navbar.jsx
@@ -4,20 +4,20 @@ export default class Navbar extends React.Component {
   render () {
     return (
       <div className="navbar-fixed">
-        <div className="row">
-          <nav className="teal">
-            <div className="nav-wrapper">
-                <div className="col s12">
-                  <a href="#" className="brand-logo">Logo</a>
-                  <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Some</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
-                  </ul>
-                </div>
+        <nav className="teal">
+          <div className="nav-wrapper">
+            <div className="row">
+              <div className="col s12">
+                <a href="#" className="brand-logo">Logo</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                  <li><a href="sass.html">Some</a></li>
+                  <li><a href="badges.html">Components</a></li>
+                  <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+              </div>
             </div>
-          </nav>
-        </div>
+          </div>
+        </nav>
       </div>
     );
   }
