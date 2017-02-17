2dd354660407af0c4f61d2c1af23a8edfe41f20e
Eric Metens <em@bolt.com>
2017-02-16 21:40:35 -0800

    fixes

--- src/client/app/components/wifi.jsx
+++ src/client/app/components/wifi.jsx
@@ -5,14 +5,14 @@ export default class WiFi extends React.Component {
   render () {
     return (
       <div>
-        <div className="col s12 m6 l4">
+        <div className="col s12 m6 l3">
           <div className="card">
             <div className="card-image">
               <img src={office} />
               <span className="card-title">WiFi</span>
             </div>
             <div className="card-content">
-              <p className="flow-text">A solid WiFi network is at what makes your Smart Home tick</p>
+              <p className="flow-text">A solid WiFi network is what makes your Smart Home tick</p>
             </div>
           </div>
         </div>
