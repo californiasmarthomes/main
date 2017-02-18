7af58a9113915d1210f643347d3e50cd9ee19853
Eric Metens <em@bolt.com>
2017-02-17 17:52:02 -0800

    media queries

---
+++ src/client/app/components/footer.jsx
@@ -0,0 +1,11 @@
+import {render} from 'react-dom';
+
+export default class Footer extends React.Component {
+  render () {
+    return (
+      <div className="footer">
+        something with a backgrounddd
+      </div>
+    );
+  }
+}
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,6 +2,7 @@ import {render} from 'react-dom';
 import Navbar from 'components/navbar';
 import Intro from 'components/intro';
 import Cards from 'components/cards';
+import Footer from 'components/footer';
 
 require('./stylesheets/main.scss');
 
@@ -11,6 +12,7 @@ class App extends React.Component {
       <div>
         <Intro />
         <Cards />
+        <Footer />
       </div>
     );
   }
