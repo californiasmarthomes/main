3febae8a6dcb1d412ef823980f086d46d60e264d
Eric Metens <em@bolt.com>
2017-03-01 21:30:26 -0800

    intro images

--- src/client/app/components/intro.jsx
+++ src/client/app/components/intro.jsx
@@ -1,30 +1,32 @@
 import {render} from 'react-dom';
-import SlideImg1 from '../images/intro_background.jpg';
+import SlideImg1 from '../images/tv.jpg';
+import SlideImg2 from '../images/livingroom.jpg';
+import SlideImg3 from '../images/internet.jpg';
 
 export default class Intro extends React.Component {
   render () {
     return (
-      <div className="slider">
+      <div className="intro slider">
         <ul className="slides">
           <li>
             <img src={SlideImg1}/>
-            <div className="caption center-align">
-              <h3>Bring the Smarts into your Home</h3>
-              <h5 className="light grey-text text-lighten-3">California Smart Home. Installation and Technology Consulting.</h5>
+            <div className="caption left-align">
+              <div className="title">Bring the Smarts into your Home</div>
+              <div className="content light grey-text text-lighten-3">Installation and Technology Consulting</div>
             </div>
           </li>
           <li>
-            <img src={SlideImg1}/>
+            <img src={SlideImg2}/>
             <div className="caption right-align">
-              <h3>Remodeling</h3>
-              <h5 className="light grey-text text-lighten-3">No project is too big or too small.</h5>
+              <div className="title">Remodeling</div>
+              <div className="content light grey-text text-lighten-3">No project is too big or too small</div>
             </div>
           </li>
           <li>
-            <img src={SlideImg1}/>
-            <div className="caption center-align">
-              <h3>Technology Consulting</h3>
-              <h5 className="light grey-text text-lighten-3">Guiding your through the complex of Smart Home options.</h5>
+            <img src={SlideImg3}/>
+            <div className="caption left-align">
+              <div className="title">Technology Consulting</div>
+              <div className="content light grey-text text-lighten-3">Guiding your through the complex world of Smart Home options</div>
             </div>
           </li>
         </ul>
